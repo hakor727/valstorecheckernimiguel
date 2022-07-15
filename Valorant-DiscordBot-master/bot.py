ec8e92b4eb07335d2d83f2ad658ed41a9e90f47d
hakor727 <109318389+hakor727@users.noreply.github.com>
2022-07-15 15:59:35 +0800

    Update bot.py

--- Valorant-DiscordBot-master/bot.py
+++ Valorant-DiscordBot-master/bot.py
@@ -59,7 +59,7 @@ class ValorantBot(commands.Bot):
         
         # bot presence
         activity_type = discord.ActivityType.playing
-        await self.change_presence(activity=discord.Activity(type=activity_type, name="Homophobia"))
+        await self.change_presence(activity=discord.Activity(type=activity_type, name="VALORANT"))
     
     async def setup_hook(self) -> None:
         if self.session is None:
